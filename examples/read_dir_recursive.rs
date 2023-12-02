079e5e45a712d30eb42fb8ad6d8a071dbd4a2981
Juanma <juan.mc.dev@gmail.com>
2023-12-02 02:52:23 -0300

    improve read_dir_recursive example

--- examples/read_dir_recursive.rs
+++ examples/read_dir_recursive.rs
@@ -1,5 +1,4 @@
 use itfs::rdr::read_dir_recursive;
-use std::time;
 
 /// ## Run this example
 ///
@@ -7,24 +6,14 @@ use std::time;
 /// cargo run --example read_dir_recursive
 /// ```
 fn main() {
-    let path = std::path::PathBuf::from(".");
-
-    let iterator = read_dir_recursive(&path).unwrap();
-    let iter_started = time::Instant::now();
-
-    println!("Started at root {:?}..", path.as_os_str());
-
-    for (i, r) in iterator.enumerate() {
+    for (i, r) in read_dir_recursive(".").unwrap().enumerate() {
         match r {
             Ok(entry) => {
-                println!("{} Found file: '{:?}'", i, entry.path());
+                println!("{} Found entry: '{:?}'", i, entry.path());
             }
             Err(e) => {
                 eprintln!("{} Found ERROR: {}", i, e);
             }
         }
     }
-
-    let elapsed = time::Instant::now().duration_since(iter_started);
-    println!("Finished. Iteration took {elapsed:?} \n");
 }
