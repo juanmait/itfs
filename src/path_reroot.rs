c16f0cc59dc93bde59fabffa5b9f94b9ce81d364
Juanma <juan.mc.dev@gmail.com>
2023-12-16 13:15:05 -0300

    rename fn re_root to path_re_root

--- src/path_reroot.rs
+++ src/path_reroot.rs
@@ -5,7 +5,7 @@
 use std::path::{Path, PathBuf, StripPrefixError};
 
 /// function that performs the prefix replacement
-pub fn re_root<P>(path: P, find: P, replace_by: P) -> Result<PathBuf, StripPrefixError>
+pub fn path_re_root<P>(path: P, find: P, replace_by: P) -> Result<PathBuf, StripPrefixError>
 where
     P: AsRef<Path>,
 {
@@ -36,7 +36,7 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.inner_iter.next() {
-            Some(p) => Some(re_root(
+            Some(p) => Some(path_re_root(
                 p.as_path(),
                 self.strip_prefix.as_ref(),
                 self.replace_by.as_ref(),
@@ -50,7 +50,7 @@ where
 mod test {
     use std::path::{Path, PathBuf};
 
-    use super::re_root;
+    use super::path_re_root;
 
     struct Subject<P: AsRef<Path>> {
         path: P,
@@ -99,7 +99,7 @@ mod test {
         ];
 
         for subject in subjects {
-            let res = re_root(subject.path, subject.strip_prefix, subject.replace_prefix);
+            let res = path_re_root(subject.path, subject.strip_prefix, subject.replace_prefix);
 
             match subject.expect.is_some() {
                 true => assert_eq!(res, Result::Ok(subject.expect.unwrap())),
