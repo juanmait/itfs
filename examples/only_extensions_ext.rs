06d2ac6f56d847c5580723ccdd01363ab2258ac5
Juanma <juan.mc.dev@gmail.com>
2024-01-13 15:57:15 -0300

    fix example only_extensions_ext

--- examples/only_extensions_ext.rs
+++ examples/only_extensions_ext.rs
@@ -9,8 +9,7 @@ fn main() {
     let allowed_extensions = vec![OsStr::new("md"), OsStr::new("toml")];
 
     // Support iterators over items of type: [`DirEntry`] ...
-    let orig_iter =
-        ResultFilter(read_dir_recursive(root_path).unwrap()).only_extensions(&allowed_extensions);
+    let orig_iter = ResultFilter(read_dir_recursive(root_path).unwrap());
     let next_iter = orig_iter.only_extensions(&allowed_extensions);
 
     for item in next_iter {
